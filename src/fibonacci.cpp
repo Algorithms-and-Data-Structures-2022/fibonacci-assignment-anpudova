05221c65ff1bb939eaf9093cb717d7cdd74239b1
Anzhela <anpudovaa@yandex.ru>
2022-04-24 13:55:42 +0300

    homework done

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -10,7 +10,7 @@ namespace assignment {
     if (n <= 1) {
       return n;
     }
-    return pow((1 + sqrt(5)) / 2, n) - pow((1 - sqrt(5)) / 2, n) / sqrt(5);
+    return (pow((1 + sqrt(5)) / 2, n) - pow((1 - sqrt(5)) / 2, n)) / sqrt(5);
   }
 
   int64_t fib_iterative(int n) {
